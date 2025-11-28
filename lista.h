4efe8d7cf537e552eee486cc04087ec14d30b55f
AArtin001 <121773044+AArtin001@users.noreply.github.com>
2025-11-28 01:04:34 -0300

    Add files via upload

--- lista.h
+++ lista.h
@@ -3,7 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "universales.h"
 
+///Declaracion de estructuras
+typedef tNodo *tLista;
 
+///Firma de funciones
+int crearLista(tLista *pL);
+
+int InsertarPriemroEnLista(tLista *pL, void *info, unsigned tamInfo);
+
+int InsertarUltimoEnLista(tLista *pL, void *info, unsigned tamInfo);
+
+int InsertarOrdenadoEnLista(tLista *pL, void *info, unsigned tamInfo, int (*cmp)(void*, void*), int(*accion)(void*, void*));
+
+int listaLlena(unsigned tamInfo);
+
+int verPrimeroLista(tLista *pL, void *info, unsigned tamInfo);
+
+int verUltimoLista(tLista *pL, void *info, unsigned tamInfo);
+
+int verPorClaveLista(tLista *pL, void *info, unsigned tamInfo, int (*cmp)(void*, void*));
+
+int sacarDeLista(tLista *pL, void *info, unsigned tamInfo, int (*cmp)(void*, void*));
+
+int listaVacia(tLista *pL);
+
+int destruirLista(tLista *pL);
 
 #endif // LISTA_H_INCLUDED
